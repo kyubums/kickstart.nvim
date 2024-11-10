ef83efa015f28bb16bdc73dd7e3726a490b65ee9
kyubum <bbulbum@gmail.com>
2024-11-11 02:20:07 +0900

    modular, add plugins

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,32 @@
+local function on_attach(bufnr)
+  local api = require 'nvim-tree.api'
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', 'l', api.node.open.edit, { desc = 'open' })
+  vim.keymap.set('n', 'h', api.node.navigate.parent_close, { desc = 'open' })
+end
+
+return {
+  'nvim-tree/nvim-tree.lua',
+  keys = function()
+    local api = require 'nvim-tree.api'
+
+    return {
+      {
+        '<leader>e',
+        function()
+          api.tree.toggle {
+            find_file = true,
+            update_root = { enable = true },
+          }
+        end,
+        mode = 'n',
+        desc = 'NvimTreeFocus',
+      },
+    }
+  end,
+  opts = {
+    on_attach = on_attach,
+  },
+}
